c3fe043275a5efc84f5bf1ce1732023a027395f4
maaliksaad <2021-bs-cs-154@tuf.edu.pk>
2024-07-16 05:09:30 +0500

    updates the portfolio and changed the ui completely

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      fontFamily: {
-        inter: ["var(--font-inter)"],
-      },
       colors: {
-        background: "rgb(var(--background))",
-        foreground: "rgb(var(--foreground))",
-        muted: "rgb(var(--muted))",
-        accent: "rgb(var(--accent))",
-      },
-      backgroundImage: {
-        "firefly-radial":
-          "radial-gradient(50% 50% at 50% 50%, rgba(253, 255, 80, 0.5) 0%, rgba(217,217,217, 0) 100%)",
-      },
-      boxShadow: {
-        "glass-inset": "inset 0 17px 5px -9px rgba(254,254,91, 0.05)",
-        "glass-sm": "5px 5px 20px 0px rgba(254,254,91, 0.3)",
-      },
-      keyframes: {
-        "spin-reverse": {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(-360deg)" },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        "big-size": {
-          "0%": { scale: 1 },
-          "100%": { scale: 1.3 },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-      },
-      animation: {
-        "spin-slow": "spin 40s linear infinite",
-        "spin-slow-reverse": "spin-reverse 40s linear infinite",
-        "pop-up": "big-size 20s ",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
       },
       screens: {
-        xs: "480px",
+        xs: "360px",
+      },
+      dropShadow: {
+        accent: "0 0 1em hsl(var(--accent))",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
